fe9c450d7fa76ec2ace7bdf72bc3e6cadb412e27
Sean Michael <seanlasono@gmail.com>
2023-09-14 08:34:18 +0300

    updated project WIP placeholder

---
+++ components/Projects.tsx
@@ -0,0 +1,15 @@
+import {Box, Center, Container, Text} from '@chakra-ui/react'
+
+export default function Projects() {
+  return (
+    <>
+      <Box pt={'60px'}></Box>
+      
+      <Container>
+        <Center p={'50%'}>
+          WIP
+        </Center>
+      </Container>
+    </>
+  )
+}
