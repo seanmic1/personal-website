35b7bb9631e720011d14efcca450d4ee9cabf692
Sean Michael <seanlasono@gmail.com>
2023-09-14 08:34:04 +0300

    updated profile message

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -40,46 +40,46 @@ const skills = [
 
 export default function Profile() {
   return (
-    <Container py={{base:'100px', md:'20px'}}>
-        <Stack direction={{base:'column', md:'row'}} w={'100%'} justify={'center'}>
-          <Image src='/SeanPic_cutout.png' alt='Picture of Sean' height={'30rem'} width={'20rem'} objectFit={'cover'} objectPosition={'15px 0px'} p={4} borderRadius={'full'} ></Image>
-          <Center p={4}>
-            <Stack textAlign={'center'}>
-              <Heading fontSize={'3xl'}>About me</Heading>
-              <Text color={useColorModeValue('gray.600','gray.400')} fontSize={'l'}>
-              The page where I talk about myself
-              </Text>
-            </Stack>
-          </Center>
-        </Stack>
-        <Stack direction={{base:'column', md:'row'}} w={'100%'} justify={'center'} p={10}>
-          <Box p={4} w={{base:'full', md:'40%'}}>
-            <Heading fontSize={'2xl'}>
-              Hey! Thanks for visiting this page.
-            </Heading>
-            <Text letterSpacing={'wide'} lineHeight={'6'} pt={4}>
-            I am Sean Michael, a Computer Science Graduate currenty based in Doha, Qatar, but willing to relocate anywhere for a job. 
-            <br></br>
-            I consider myself a casual coder, but I&apos;m now actively working on digging into Web Dev and ML technologies as a hobby.
-            <br></br>
-            Aside from that, my hobbies are gaming and playing music. I&apos;m currently learning the saxophone as well
-            {/* <br></br>
-            I'm naturally a curious person, currently learning all sorts of frameworks or technologies available on the web. I learn fast and adapt even faster, something that is needed in today's software development landscape. If you think I'd be a good fit to your team, you can contact me here. */}
+    <Container maxW={'container.lg'} py={{base:'100px', md:'20px'}} centerContent>
+      <Stack direction={{base:'column', md:'row'}}>
+        <Image src='/SeanPic_cutout.png' alt='Picture of Sean' height={'30rem'} width={'20rem'} objectFit={'cover'} objectPosition={'15px 0px'} p={4} borderRadius={'full'} ></Image>
+        <Center p={4}>
+          <Stack textAlign={'center'}>
+            <Heading fontSize={'3xl'}>About me</Heading>
+            <Text color={useColorModeValue('gray.600','gray.400')} fontSize={'l'}>
+            The page where I talk about myself
             </Text>
-          </Box>  
-          <Box ml={'6rem'} p={4} w={'40%'} >
-            <Heading fontSize={'2xl'}>
-              My skills
-            </Heading>
-            <Wrap py={4}>
-                {skills.map((skill) => 
-                (<WrapItem key={skill}>
-                    <Tag size={'lg'} mx={2}>{skill}</Tag>
-                  </WrapItem>
-                ))}
-            </Wrap>
-          </Box>
-        </Stack>
+          </Stack>
+        </Center>
+      </Stack>
+      <Stack direction={{base:'column', md:'row'}} w={'100%'} justify={'center'} pt={'5rem'}>
+        <Box p={4}>
+          <Heading fontSize={'2xl'}>
+            Hey! Thanks for visiting.
+          </Heading>
+          <Text letterSpacing={'wide'} lineHeight={'6'} pt={4}>
+          I am Sean Michael, a Computer Science Graduate currenty based in Doha, Qatar.
+          <br></br>
+          I am working on some pet projects as a pass time as of now, including this website!
+          <br></br>
+          Aside from that, my hobbies are Dota 2 and playing music, particularly saxophone.
+          {/* <br></br>
+          I'm naturally a curious person, currently learning all sorts of frameworks or technologies available on the web. I learn fast and adapt even faster, something that is needed in today's software development landscape. If you think I'd be a good fit to your team, you can contact me here. */}
+          </Text>
+        </Box>  
+        <Box p={4}>
+          <Heading fontSize={'2xl'}>
+            My skills
+          </Heading>
+          <Wrap py={4}>
+              {skills.map((skill) => 
+              (<WrapItem key={skill}>
+                  <Tag size={'lg'} mx={2}>{skill}</Tag>
+                </WrapItem>
+              ))}
+          </Wrap>
+        </Box>
+      </Stack>
     </Container>
   )
 }
