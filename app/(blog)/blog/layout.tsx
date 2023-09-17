f7d73b094125aeacac50399e5a4b564db8791247
Sean Michael <seanlasono@gmail.com>
2023-09-17 21:48:49 +0300

    added google analytics to blog pages

--- app/(blog)/blog/layout.tsx
+++ app/(blog)/blog/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 
 import "@/app/global_components/globals.css"
+import Script from "next/script"
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -18,6 +19,15 @@ export default function RootLayout({
   return (
       <html lang="en">
         <head>
+          <Script id="GoogleAnalytics">
+            {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-N9WRVZLC');
+            `}
+          </Script>
             <link rel="icon" type="image/png" sizes="any" href="/seanml_tp.png"/>
         </head>
         <body className="dark:bg-slate-800">
