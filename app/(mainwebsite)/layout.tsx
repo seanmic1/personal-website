70633e1eb4dde335dfe32b2543565e322c032f78
Sean Michael <seanlasono@gmail.com>
2023-09-17 16:23:54 +0300

    replaced gtag with GTM

--- app/(mainwebsite)/layout.tsx
+++ app/(mainwebsite)/layout.tsx
@@ -21,26 +21,27 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <Script>
+          {`
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-N9WRVZLC');
+          `}
+        </Script>
           <link rel="icon" type="image/png" sizes="any" href="/seanml_tp.png"/>
       </head>
       <body className={inter.className}>
-        <div className="container">
-          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-W1WZS731E2" />
-          <Script id="google-analytics">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-            
-              gtag('config', 'G-W1WZS731E2');
-            `}
-          </Script>
-        </div>
         <ClientComponent>
           <Navbar></Navbar>
           {children}
           <Footer></Footer>
         </ClientComponent>
+        <noscript dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9WRVZLC" height="0" width="0" style="display: none; visibility: hidden;"></iframe>`,
+          }}
+        />
       </body>
     </html>
   )
