4ce35ebf3c392a2706c42d06cae59745650da8bb
Sean Michael <seanlasono@gmail.com>
2023-09-18 00:48:57 +0300

    moved resume to component and added metadata

---
+++ app/(mainwebsite)/components/Resume.tsx
@@ -0,0 +1,39 @@
+"use client"
+
+import { Button, Flex } from "@chakra-ui/react";
+import {saveAs} from "file-saver";
+
+
+export default function ResumePage() {
+
+  const saveFile = () => {
+    saveAs(
+      "/SeanResumeNew.pdf",
+      "SeanResume.pdf"
+    );
+  };
+
+  return(
+     <Flex h="100vh" justifyContent="center" alignItems="center">
+
+      <Button
+        onClick={saveFile}
+        px={4}
+        fontSize={'sm'}
+        rounded={'full'}
+        bg={'blue.400'}
+        color={'white'}
+        boxShadow={
+          '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
+        }
+        _hover={{
+          bg: 'blue.500',
+        }}
+        _focus={{
+          bg: 'blue.500',
+        }}>
+        Download resume
+      </Button>
+    </Flex>
+  )
+}
--- app/(mainwebsite)/resume/page.tsx
+++ app/(mainwebsite)/resume/page.tsx
@@ -1,39 +1,12 @@
-"use client"
+import ResumePage from "../components/Resume";
+import { Metadata } from 'next'
 
-import { Button, Flex } from "@chakra-ui/react";
-import {saveAs} from "file-saver";
+export const metadata: Metadata = {
+  title: 'Resume Page',
+}
 
-
-export default function ResumePage() {
-
-  const saveFile = () => {
-    saveAs(
-      "/SeanResumeNew.pdf",
-      "SeanResume.pdf"
-    );
-  };
-
-  return(
-     <Flex h="100vh" justifyContent="center" alignItems="center">
-
-      <Button
-        onClick={saveFile}
-        px={4}
-        fontSize={'sm'}
-        rounded={'full'}
-        bg={'blue.400'}
-        color={'white'}
-        boxShadow={
-          '0px 1px 25px -5px rgb(66 153 225 / 48%), 0 10px 10px -5px rgb(66 153 225 / 43%)'
-        }
-        _hover={{
-          bg: 'blue.500',
-        }}
-        _focus={{
-          bg: 'blue.500',
-        }}>
-        Download resume
-      </Button>
-    </Flex>
+export default function Resume() {
+  return (
+    <ResumePage/>
   )
 }
