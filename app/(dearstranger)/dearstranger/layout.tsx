fe0d98054b88b40a54b52b472ffd3981ea44a747
Sean Michael <seanlasono@gmail.com>
2023-09-17 15:23:36 +0300

    deleted dearstranger folders

--- app/(dearstranger)/dearstranger/layout.tsx
+++
@@ -1,7 +0,0 @@
-export default function DearLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return <section>{children}</section>
-}
--- app/(dearstranger)/dearstranger/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function DearStranger() {
-  return (
-    <>
-    YESS
-    </>
-  )
-}
